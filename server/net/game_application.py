efea569826b46dd6115e1d0a9664b323e4d27318
grozamorei <desperatebassplayer@gmail.com>
2015-04-19 18:35:29 +0300

    made static data serving

--- server/net/game_application.py
+++ server/net/game_application.py
@@ -1,13 +1,16 @@
 import logging
+import tornado
+import os
 from tornado.web import Application
 from net.ws_handler import WSHandler
 
 LOGGER = logging.getLogger(__name__.split('.')[-1])
 
-
 class GameApplication(Application):
     HANDLERS = [
-        (r'/echo', WSHandler)
+        (r'/echo', WSHandler),
+        (r'/static/(.*)', tornado.web.StaticFileHandler, {'path': os.path.join(os.getcwd(), 'static')})
+
     ]
 
     def __init__(self, cluster):
